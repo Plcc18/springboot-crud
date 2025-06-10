46bb6d3dbc943f7f0b7e6c8fb241cffc21151549
Pedro <pedrolucascabral4@gmail.com>
2025-06-10 16:18:14 -0300

    configurando entidade produto

--- src/main/java/com/example/springboot_crud/model/Produto.java
+++ src/main/java/com/example/springboot_crud/model/Produto.java
@@ -1,5 +1,39 @@
 package com.example.springboot_crud.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Produto {
-  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private double preco;
+
+    //Getter e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
 }
