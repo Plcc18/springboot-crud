dcc905fea8f4307ccf49ba5f663b5daed34e02a3
Pedro <pedrolucascabral4@gmail.com>
2025-06-11 18:52:27 -0300

    Alteração de comentário da Interface

--- src/main/java/com/example/springboot_crud/repository/ProdutoRepository.java
+++ src/main/java/com/example/springboot_crud/repository/ProdutoRepository.java
@@ -3,7 +3,7 @@ package com.example.springboot_crud.repository;
 import com.example.springboot_crud.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//Herança da interface JpaRepository
+//Implementa a interface interface JpaRepository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 }
