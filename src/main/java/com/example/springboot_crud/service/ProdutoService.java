5d9900530f86f5dd323db08113116da6007a93a4
Pedro <pedrolucascabral4@gmail.com>
2025-06-10 22:21:43 -0300

    Service para regras de négocios para o produto

--- src/main/java/com/example/springboot_crud/service/ProdutoService.java
+++ src/main/java/com/example/springboot_crud/service/ProdutoService.java
@@ -1,5 +1,35 @@
 package com.example.springboot_crud.service;
 
+import com.example.springboot_crud.model.Produto;
+import com.example.springboot_crud.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+//Regra de negócios
+@Service
 public class ProdutoService {
-  
+
+    private final ProdutoRepository repository;
+
+    public ProdutoService(ProdutoRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Produto> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<Produto> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public Produto salvar(Produto produto) {
+        return repository.save(produto);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
 }
