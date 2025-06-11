360252c003b67f00ea5223bc13b819cb11863d51
Pedro <pedrolucascabral4@gmail.com>
2025-06-10 23:23:27 -0300

    Construtor ProdutoController

--- src/main/java/com/example/springboot_crud/controller/ProdutoController.java
+++ src/main/java/com/example/springboot_crud/controller/ProdutoController.java
@@ -1,5 +1,16 @@
 package com.example.springboot_crud.controller;
 
+import com.example.springboot_crud.service.ProdutoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
-  
+
+    private final ProdutoService service;
+
+    public ProdutoController(ProdutoService service) {
+        this.service = service;
+    }
 }
