a05423c75380ec9f8037e044fba1eb55691070a9
Pedro <pedrolucascabral4@gmail.com>
2025-06-10 23:26:02 -0300

    Rota listarTodos

--- src/main/java/com/example/springboot_crud/controller/ProdutoController.java
+++ src/main/java/com/example/springboot_crud/controller/ProdutoController.java
@@ -1,16 +1,26 @@
 package com.example.springboot_crud.controller;
 
+import com.example.springboot_crud.model.Produto;
 import com.example.springboot_crud.service.ProdutoService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
+    //Instância Service
     private final ProdutoService service;
 
     public ProdutoController(ProdutoService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public List<Produto> listarTodos() {
+        return service.listarTodos();
     }
 }
